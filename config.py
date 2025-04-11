0079e979c25e1d591aeb96a97e864d8672b43477
LYXKZ <golddropperofficial@gmail.com>
2025-04-10 22:23:22 -0400

    update 2

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
 
 # remote db
-SQLALCHEMY_DATABASE_URI = "postgresql://neondb_owner:npg_1cqyZKNXgku9@ep-orange-bonus-a5acoeh0-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require"
-SECRET_KEY = "my_secret_key"  # Change this for security
+# THIS IS A PUBLIC DEMO BRANCH
+SQLALCHEMY_DATABASE_URI = "postgresql://neondb_owner:npg_1cqyZKNXgku9@ep-fancy-star-a5co9tf8-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require"
+
+SECRET_KEY = "my_secret_key"  
